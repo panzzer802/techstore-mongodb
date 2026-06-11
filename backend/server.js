ee5d9a15bac4d89227537165a54609de12e6670f
panzzer802 <jaimitojamonsalchicha@gmail.com>
2026-06-11 08:09:24 -0700

    Configurar puerto para Render

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,8 @@ app.get('/', (req, res) => {
     res.send('API de TechStore funcionando');
 });
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en puerto 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
 });
